db8b0c26fc25953af6a6bb7b2df1a8e98886e01d
Priyanshukr766 <priyanshukumarnk111@gmail.com>
2025-03-01 15:18:33 +0530

    finalllllllll code update

--- Priyanshu/SQL_Table.sql
+++ Priyanshu/SQL_Table.sql
@@ -6,8 +6,6 @@ CREATE TABLE CUSTOMERS (
     customer_id VARCHAR(50) PRIMARY KEY,
     name VARCHAR(100) UNIQUE NOT NULL,
     phone VARCHAR(10) UNIQUE NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    phone VARCHAR(10) UNIQUE NOT NULL, //only one values needed
     email VARCHAR(100) UNIQUE NOT NULL,
     address VARCHAR(150)
     );
@@ -34,7 +32,8 @@ CREATE TABLE ORDERS (
     customer_id VARCHAR(50) NOT NULL,
     staff_id VARCHAR(50),
     order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    status ENUM('pending', 'in_progress', 'completed', 'cancelled') NOT NULL DEFAULT 'pending',
+    Final_Total FLOAT NOT NULL,
+   
     
     FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id) ON DELETE CASCADE,
     FOREIGN KEY (staff_id) REFERENCES STAFF(staff_id) ON DELETE SET NULL
@@ -62,36 +61,6 @@ CREATE TABLE PAYMENTS (
 );
 
 
-DELIMITER $$
 
-CREATE TRIGGER update_total_cost
-AFTER INSERT ON ORDER_DETAILS
-FOR EACH ROW
-BEGIN
-    UPDATE ORDERS
-    SET total_cost = (
-        SELECT COALESCE(SUM(subtotal), 0) 
-        FROM ORDER_DETAILS 
-        WHERE order_id = NEW.order_id
-    )
-    WHERE order_id = NEW.order_id;
-END $$
-
-DELIMITER ;
-
-
-DELIMITER $$
-
-CREATE TRIGGER create_payment_entry
-AFTER INSERT ON ORDERS
-FOR EACH ROW
-BEGIN
-    INSERT INTO PAYMENTS (payment_id, order_id, amount, payment_mode, status)
-    VALUES (
-        UUID(), NEW.order_id, NEW.total_cost, 'cash', 'pending'
-    );
-END $$
-
-DELIMITER ;
 
 
